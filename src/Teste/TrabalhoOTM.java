221c40444bc23f83fd8dd16584dee962b6127878
nathapaulino <nathapaulino17@gmail.com>
2019-05-30 06:10:57 -0300

    Starting with a test model

---
+++ src/Teste/TrabalhoOTM.java
@@ -0,0 +1,15 @@
+package Teste;
+
+import java.util.*;
+import ilog.concert.*;
+import ilog.cplex.*;
+
+public class TrabalhoOTM {
+	/*
+	 * Classe feita para chamar outros modelos existentes e tratar os arquivos .csv lidos 
+	 */
+	public static void main(String[] args) {
+	
+		Test.model();
+	}	
+}
---
+++ src/Teste/UnbalancedModel.java
@@ -0,0 +1,10 @@
+package Teste;
+
+import java.util.*;
+import ilog.concert.*;
+import ilog.cplex.*;
+
+public class UnbalancedModel {
+	
+	
+}
